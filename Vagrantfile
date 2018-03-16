411f4f578adad97e43d9775cc362a2fab90b8431
Immanuel Potter <mannypotter@hotmail.com>
2018-03-16 14:25:43 +0000

    Tidied up, still have pam configuration to fix properly - not changing /etc/pam.d/su correctly with starting comment

--- Vagrantfile
+++ Vagrantfile
@@ -64,7 +64,6 @@ Vagrant.configure(2) do |config|
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
   # config.vm.provision "shell", inline: <<-SHELL
-  #   sudo apt-get update
-  #   sudo apt-get install -y apache2
+  #  yum update -y && yum upgrade -y
   # SHELL
 end
--- pam_configuration.sh
+++ pam_configuration.sh
@@ -16,23 +16,23 @@ pam_configure(){
 }
 
 pam_pw_lockouts(){
-  grep_check "auth required pam_faillock.so preauth audit silent unlock_time=900" /etc/pam.d/password-auth 'auth required pam_faillock.so preauth audit silent=900'
+  grep_check "^auth required pam_faillock.so preauth audit" /etc/pam.d/password-auth 'auth required pam_faillock.so preauth audit silent deny=5 unlock_time=900'
   grep_check "auth [success=1 default=bad] pam_unix.so" /etc/pam.d/system-auth 'auth [success=1 default=bad] pam_unix.so'
   grep_check "auth [default=die] pam_faillock.so authfail audit unlock_time=900" /etc/pam.d/system-auth 'auth [default=die] pam_faillock.so authfail audit unlock_time=900'
   grep_check "auth sufficient pam_faillock.so authsucc audit unlock_time=900" /etc/pam.d/system-auth 'auth sufficient pam_faillock.so authsucc audit unlock_time=900'
 }
 
 pam_pw_remember(){
-  grep_check "^password\s+sufficient\s+pam_unix.so" /etc/pam.d/password-auth 'password sufficient pam_unix.so remember=5'
-  grep_check "^password\s+sufficient\s+pam_unix.so" /etc/pam.d/system-auth 'password sufficient pam_unix.so remember=5'
+  grep_check "^password\s+sufficient\s+pam_unix.so\s+remember" /etc/pam.d/password-auth 'password sufficient pam_unix.so remember=5'
+  grep_check "^password\s+sufficient\s+pam_unix.so\s+remember" /etc/pam.d/system-auth 'password sufficient pam_unix.so remember=5'
 }
 
 pam_pw_sha512(){
-  grep_check "^password\s+sufficient\s+pam_unix.so" /etc/pam.d/password-auth 'password sufficient pam_unix.so sha512'
-  grep_check "^password\s+sufficient\s+pam_unix.so" /etc/pam.d/system-auth 'password sufficient pam_unix.so sha512'
+  grep_check "^password\s+sufficient\s+pam_unix.so\s+sha" /etc/pam.d/password-auth 'password sufficient pam_unix.so sha512'
+  grep_check "^password\s+sufficient\s+pam_unix.so\s+sha" /etc/pam.d/system-auth 'password sufficient pam_unix.so sha512'
 }
 
 
 pam_su_restriction(){
-  grep_check "pam_wheel.so" /etc/pam.d/su 'auth required pam_wheel.so use_uid'
+  grep_check "auth\s+required\s+pam_wheel.so" /etc/pam.d/su 'auth required pam_wheel.so use_uid'
 }
