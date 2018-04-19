012ccf70e6643304fc75427fbab9115f8ed2f023
Immanuel Potter <immanuel.potter@homeoffice.gsi.gov.uk>
2018-04-19 17:13:44 +0100

    Added root user to aide crontab

--- crontab_aide.sh
+++ crontab_aide.sh
@@ -2,5 +2,5 @@
 
 aide_crontab="$(grep "aide" /etc/crontab)"
 if [[ -z "$aide_crontab" ]] ; then
-  echo "0 5 * * * /sbin/aide --check" >> /etc/crontab
+  echo "0 5 * * * root /sbin/aide --check" >> /etc/crontab
 fi
