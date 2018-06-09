f1011f022deed71944e93b130fdcadea73fe8037
immanuelpotter <mannypotter@hotmail.com>
2018-06-09 23:55:58 +0100

    Made output less noisy for file checking

--- finds.sh
+++ finds.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
 find_ww_dirs(){
-  echo -e "World-writable check\nThe following dirs: \n$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -0002) \nare world-writable. Check this is supposed to be the case, and if not, remove with chmod o-w." 
+  echo "Performing checks on world-writable files..."
+  local BAD="$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -0002)"
+  if [[ ${#BAD} -gt 0 ]] ; then
+      echo -e "The following files: \n ${BAD} \nare world-writable. Check this is supposed to be the case, and if not, remove with chmod o-w."
+  fi
 }
 
 find_unowned_files_and_dirs(){
-  echo -e "Unowned check\nThe following files and dirs:\n$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -nouser) \nhave no owner. Check this is supposed to be the case, and if not, remove them - as new users who may be assigned the old user ID may have more access than intended." 
+  echo "Performing checks on unowned files and directories..."
+  local BAD="$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -nouser)"
+  if [[ ${#BAD} -gt 0 ]] ; then
+    echo -e "The following files and dirs:\n ${BAD} \nhave no owner. Check this is supposed to be the case, and if not, remove them - as new users who may be assigned the old user ID may have more access than intended."
+  fi
 }
 
 find_ungrouped_files_and_dirs(){
-  echo -e "Ungrouped check\nThe following files and dirs:\n$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -nogroup) \nhave no group. Check this is supposed to be the case, and if not, remove them - as new users who may be assigned the old group ID may have more access than intended." 
+  echo -e "Performing checks on ungrouped files and directories..."
+  local BAD="$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -nogroup)"
+  if [[ ${#BAD} -gt 0 ]] ; then
+    echo -e "The following files and dirs:\n ${BAD} \nhave no group. Check this is supposed to be the case, and if not, remove them - as new users who may be assigned the old group ID may have more access than intended."
+  fi
 }
 
 audit_suid_executables(){
-  echo -e "SUID Audit check\nThe following files:\n$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -4000) \nhave SUID set. Check this is supposed to be the case, and if not, remove - as users may be able to change their UID and may have more access than intended." 
+  echo -e "Performing checks on suid executables..."
+  local BAD="$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -4000)"
+  if [[ ${#BAD} -gt 0 ]] ; then
+    echo -e "The following files:\n ${BAD} \nhave SUID set. Check this is supposed to be the case, and if not, remove - as users may be able to change their UID and may have more access than intended."
+  fi
 }
 
 audit_sgid_executables(){
-echo -e "SGID Audit check\nThe following files:\n$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000) \nhave SGID set. Check this is supposed to be the case, and if not, remove - as users may be able to change their GID and may have more access than intended." 
+  echo -e "Performing checks on sgid executables..."
+  local BAD="$(df --local -P | awk '(NR!=1) {print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000)"
+  if [[ ${#BAD} -gt 0 ]] ; then
+    echo -e "The follwing files:\n ${BAD} \nhave SGID set. Check this is supposed to be the case, and if not, remove - as users may be able to change their GID and may have more access than intended."
+  fi
 }
