2cdd6856f0e1ce6fed58aaa1a83013153b65fdaf
Immanuel Potter <immanuel.potter@homeoffice.gsi.gov.uk>
2018-08-07 15:23:40 +0100

    Removed stuff moved to main

--- get_me_back_in.sh
+++ get_me_back_in.sh
@@ -3,7 +3,6 @@
 # Quick utility to run post-hardening so the vagrant user can get back in the box via ssh
 
 USERS_TO_ALLOW_IN="vagrant"
-LOCAL_NETWORK:="10.0.2"
 
 add_users_to_wheel(){
 for i in $USERS_TO_ALLOW_IN ; do
