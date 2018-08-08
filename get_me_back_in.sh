44cbd335b6965a12130de401e716eb9ada16e409
Immanuel Potter <immanuel.potter@homeoffice.gsi.gov.uk>
2018-08-08 12:36:27 +0100

    Added re-entry of user to main.sh, and catered for vagrant/aws amis

--- get_me_back_in.sh
+++ get_me_back_in.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Quick utility to run post-hardening so the vagrant user can get back in the box via ssh
+# Quick utility to run post-hardening so the vagrant user can get back in the box via ssh.
+# Modify this script after the initial install to only include the correct one.
 
-USERS_TO_ALLOW_IN="vagrant"
+find_user(){
+  POTENTIAL_USERS="ec2-user vagrant"
+  for i in ${POTENTIAL_USERS} ; do 
+    if [[ cat /etc/passwd | grep ${i} ]] ; then
+      global USERS_TO_ALLOW_IN=${i}
+    fi
+  done
+}
 
 add_users_to_wheel(){
 for i in $USERS_TO_ALLOW_IN ; do
--- main.sh
+++ main.sh
@@ -343,6 +343,7 @@ main(){
   restart_services_main
   verify_system
   add-gui
+  . ./get_me_back_in.sh
   echo "Scripts finished."
   echo "Please check hardening_report.txt"
 }
